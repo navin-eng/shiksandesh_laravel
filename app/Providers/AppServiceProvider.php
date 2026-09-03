a68bc81d5fb73d8153e562ea5f3f2ad1a8dd1748
navin-eng <>
2026-09-03 09:12:52 +0545

    Share siteSettings globally to all views via View composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\SiteSetting;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            try {
+                $view->with('siteSettings', SiteSetting::current());
+            } catch (\Throwable $e) {
+                $view->with('siteSettings', new SiteSetting());
+            }
+        });
     }
 }
